118e08364386d9eaf8b6356841d659244e45e1e9
Gfc9110 <Gfc9110@gmail.com>
2016-07-17 20:41:39 +0200

    Added New Scene
    Grass And Normal Mapped Terrain

---
+++ ProjectObjectLaunch/Assets/Scripts/DrawDistance.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawDistance : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float distance = Mathf.Abs ((new Vector2(player.transform.position.x,player.transform.position.z)-new Vector2(transform.position.x,transform.position.z)).sqrMagnitude);
+
+		GetComponent<MeshRenderer> ().enabled = distance < 400;
+	
+	}
+}
---
+++ ProjectObjectLaunch/Assets/Scripts/GrassCreator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassCreator : MonoBehaviour {
+
+	public Vector2 chunkSize = new Vector2 (1,1);
+
+	public GameObject grassPiece;
+
+	public GameObject player;
+
+	public float minGrassSize = 0.8f;
+
+	public float maxGrassSize = 1.2f;
+
+	public float grassPerMeter = 100;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < (grassPerMeter * chunkSize.x * chunkSize.y); i++) {
+
+			GameObject grass = (GameObject) Instantiate (grassPiece, new Vector3(Random.Range (-chunkSize.x/2,chunkSize.x/2)+transform.position.x,transform.position.y,Random.Range (-chunkSize.y/2,chunkSize.y/2)+transform.position.z), Quaternion.Euler (-90,0,0));
+
+			float scaleFactor = Random.Range (minGrassSize, maxGrassSize);
+
+			grass.transform.localScale = new Vector3 (scaleFactor, scaleFactor, scaleFactor);
+
+			grass.GetComponent<DrawDistance> ().player = player;
+
+		}
+	
+	}
+}
